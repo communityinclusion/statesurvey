9cf7273d5cd90c525c3bf0753cbc261db0d2482e
Paul Foos <paul.foos@umb.edu>
2021-05-19 13:17:24 -0400

    styling new CIE system

--- all/modules/custom/surveynodeform/js/GeorgiaCIE.js
+++ all/modules/custom/surveynodeform/js/GeorgiaCIE.js
@@ -30,7 +30,7 @@ Drupal.behaviors.remState = {
       $( document ).one('ready',scanFieldsets);
       $(document).one('ready',surveynodeformcheckAllActions);
       $('th').text('Order').hide();
-      if (!$('#edit-group_indv_comp p.nextSec').length)$('#edit-group_indv_comp').append('<p class="nextSec"><a class="openTab" href="#edit-group_grp_integ">>> Next: Group integrated job >></a></p>');
+      if (!$('#edit-group_cie p.nextSec').length)$('#edit-group_indv_comp').append('<p class="nextSec"><a class="openTab" href="#edit-group_grp_integ">>> Next: Group integrated job >></a></p>');
       if (!$('#edit-group_grp_integ p.nextSec').length) $('#edit-group_grp_integ').append('<p class="nextSec"><a class="openTab" href="#edit-group_self_emp">>> Next: Self employment >></a></p>');
        if (!$('#edit-group_self_emp p.nextSec').length) $('#edit-group_self_emp').append('<p class="nextSec"><a class="openTab" href="#edit-group_shl">>> Next: Facility based/sheltered work >></a></p>');
      if (!$('#edit-group_shl p.nextSec').length) $('#edit-group_shl').append('<p class="nextSec"><a class="openTab" href="#edit-group_com_non_work">>> Next: Community based non work >></a></p>');
