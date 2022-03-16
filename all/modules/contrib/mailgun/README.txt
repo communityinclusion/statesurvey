763dff4e74955b23f244b134f0a70d8cc580a453
Paul Foos <paul.foos@umb.edu>
2022-03-16 11:24:27 -0400

    fixing bad merge

--- all/modules/contrib/mailgun/README.txt
+++
@@ -1,1 +0,0 @@
-Mailgun module documentation is here: https://www.drupal.org/node/2547591.
--- all/modules/custom/sdata_id/js/md.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-
-  /**
-   * Individual competitive job - if gross wages / hours < $8.25, then show minimum wage
-   * stuff.
-   */
-
-
-})(jQuery);
